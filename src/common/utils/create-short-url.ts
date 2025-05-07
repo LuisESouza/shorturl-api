20b8092ba1ae918722a8b143a2ea18f74be914e3
LuisESouza <leduardosouza2204@gmail.com>
2025-05-07 15:56:56 -0400

    [FEAT] Add short code generator utility

---
+++ src/common/utils/create-short-url.ts
@@ -0,0 +1,4 @@
+export function generateShortCode(length = 6) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    return Array.from({ length }, () => chars[Math.floor(Math.random() * chars.length)]).join('');
+  }  
