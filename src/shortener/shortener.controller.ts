3632ce1441ad846ceea78805c35724bb0d4fb968
LuisESouza <leduardosouza2204@gmail.com>
2025-05-10 18:08:58 -0400

    [FEAT]: Add routes for public and authenticated users

--- src/shortener/shortener.controller.ts
+++ src/shortener/shortener.controller.ts
@@ -1,16 +1,28 @@
-import { Controller, Get, Post, Body, Param, NotFoundException, Redirect } from '@nestjs/common';
-import { ShortenerService } from './shortener.service';
+import { Controller, Get, Post, Body, Param, NotFoundException, Redirect, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
 import { CreateShortUrlDto } from './dto/create-short-url.dto';
+import { ShortenerService } from './shortener.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
-@Controller('shortener')
+@Controller('')
 export class ShortenerController {
   constructor(private readonly shortenerService: ShortenerService) {}
 
-  @Post('shorten')
-  async shortenUrl(@Body() dto: CreateShortUrlDto) {
-    const shortUrl = await this.shortenerService.createShortUrl(dto.url);
+  @UseGuards(AuthGuard)
+  @Post('')
+  async shortenUrlAuth(@Body() dto: CreateShortUrlDto, @Req() request: Request) {
+    const userId = request['user'].id;
+    const shortUrl = await this.shortenerService.createShortUrl(dto.url, userId);
     return { shortUrl };
   }
+
+  @Post('guest')
+  async shortenUrlGuest(@Body() dto: CreateShortUrlDto, @Req() request: Request) {
+    const ip = (request.headers['x-forwarded-for'] as string)?.split(',')[0] || request.socket.remoteAddress;
+    const shortUrl = await this.shortenerService.createShortUrl(dto.url, undefined, ip);
+    return { shortUrl };
+  }
+
 
   @Get(':code')
   @Redirect()
