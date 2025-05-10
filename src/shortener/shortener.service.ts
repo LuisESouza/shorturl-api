f6cf2220d920fdf0d82319ea671ae219f67d7714
LuisESouza <leduardosouza2204@gmail.com>
2025-05-10 18:13:00 -0400

    [FEAT] Add daily limit for unauthenticated users

--- src/shortener/shortener.service.ts
+++ src/shortener/shortener.service.ts
@@ -5,13 +5,23 @@ import { generateShortCode } from '../common/utils/create-short-url';
 @Injectable()
 export class ShortenerService {
   constructor(private readonly prisma: PrismaService) {}
-  async createShortUrl(originalUrl: string): Promise<string> {
+
+
+  async createShortUrl(originalUrl: string, userId?: string, ip?: string): Promise<string> {
+    if (!userId && ip) {
+      const hoje = new Date();
+      hoje.setHours(0, 0, 0, 0);
+      const jaCriouHoje = await this.prisma.url.findFirst({where: {ip: ip, created_at: { gte: hoje }}});
+      if (jaCriouHoje) {throw new Error('Usuários não autenticados só podem encurtar 1 link por dia.');}
+    }
     const code = generateShortCode();
-    const exists = await this.prisma.url.findUnique({where: { shortCode: code }});
-    if (exists) return this.createShortUrl(originalUrl);
-    await this.prisma.url.create({data: {original: originalUrl, shortCode: code,}});
-    return `http://localhost:4004/shortener/${code}`;
+    const exists = await this.prisma.url.findUnique({ where: { shortCode: code } });
+    if (exists) return this.createShortUrl(originalUrl, userId, ip);
+    await this.prisma.url.create({
+      data: {original: originalUrl, shortCode: code, userId: userId, ip: ip,}});
+    return `http://localhost:4004/${code}`;
   }
+
   async getOriginalUrl(code: string): Promise<string | null> {
     const record = await this.prisma.url.findUnique({where: { shortCode: code },});
     if (!record) {throw new NotFoundException('URL não encontrada');}
